ccf3a3615eea0b418c9ef16c4544aef8ced9eb55
Aneta Maciejczak <aneta.maciejczak@gmail.com>
2018-08-30 13:54:11 +0200

    ready

--- src/modules/checkbox/index.js
+++ src/modules/checkbox/index.js
@@ -1,19 +1,62 @@
 /* eslint-disable */
 import React from 'react';
 
-class Checkbox extends React.Component {
-    render() {
-        return(
-            <div className="checkbox">
-                <div className="title">
-                    Checkbox
-                </div>
-                <div className="content">
-                    <input type="checkbox" />
-                </div>
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isChecked: props.isChecked || false,
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange() {
+        this.setState({ isChecked: !this.state.isChecked })
+    }
+    render () {
+        const content = this.state.isChecked
+            ? <div className= "content">HIDDEEN CONTENT</div>
+            : null
+
+        return (
+            <div>
+                <div className = "title">Checkbox</div>
+                <label className="switch">
+                    <input type="checkbox" value={this.state.isChecked} onChange={this.handleChange} />
+                    <div className="slider"></div>
+                </label>
+                {content}
             </div>
-        );
+    );
     }
 }
 
-export default Checkbox;
+
+
+
+//
+// class Checkbox extends React.Component {
+//     constructor (props) {
+//         super (props);
+//         // this.state = {
+//         //     isChacked:
+//         //
+//         // }
+//     }
+//     render() {
+//         // const content =
+//         return(
+//             <div className="checkbox">
+//                 <div className="title">
+//                     Checkbox
+//                 </div>
+//                 <div className="content">
+//                     <input type="checkbox" />
+//                 </div>
+//             <div>HIDDEN CONTENT</div>
+//             </div>
+//         );
+//     }
+// }
+
+export default Toggle;
